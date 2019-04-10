e798026bcceceda32e1e86500f66a10d539063df
22blackte <49483176+22blackte@users.noreply.github.com>
2019-04-10 12:46:08 -0400

    adding Hello-world.py
    done

---
+++ hello-world.py
@@ -0,0 +1,12 @@
+# Teshawn
+# 04/10/2019
+
+##################
+#    Hello       #
+#                #
+# our first python#
+#  Application   #
+##################
+
+# print "Hello, world" to the screnn
+print("Hello< world")
